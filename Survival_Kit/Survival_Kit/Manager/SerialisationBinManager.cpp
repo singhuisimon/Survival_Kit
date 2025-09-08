ab966b994edb3556d4069985b113937c1ba4d530
Low Yue Jun <>
2025-09-08 09:38:58 +0800

    Binary Serialization
    Binary serialization for components. Has not been tested for robustness, does not support maps. Can be updated to support maps in the future.

---
+++ Survival_Kit/Survival_Kit/Manager/SerialisationBinManager.cpp
@@ -0,0 +1,5 @@
+#include "SerialisationBinManager.h"
+
+// All methods are templates (or header-inline) by design.
+// This translation unit intentionally contains no out-of-line
+// definitions to avoid template/linker issues.
