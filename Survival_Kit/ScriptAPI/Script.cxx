66d6899c124d6797341f5c503e0bb697ec2aad15
literallyjoyboy <kuekweijie@gmail.com>
2025-09-23 12:57:32 +0800

    just did some works

--- Survival_Kit/ScriptAPI/Script.cxx
+++ Survival_Kit/ScriptAPI/Script.cxx
@@ -3,12 +3,92 @@
 
 namespace ScriptAPI
 {
-    TransformComponent Script::GetTransformComponent()
+    // Constructor - initialize the script
+    Script::Script()
     {
-        return TransformComponent(entityId);
+        entityId = -1;
+        isEnabled = true;
+        awakeCalled = false;
+        startCalled = false;
+        objectName = "Script";
+        objectTag = "Untagged";
     }
+
+    // Component access
+    TransformComponent^ Script::GetTransformComponent()
+    {
+        return gcnew TransformComponent(entityId);
+    }
+
     void Script::SetEntityId(int id)
     {
         entityId = id;
+    }
+
+    // Utility methods
+    bool Script::CompareTag(System::String^ tagToCompare)
+    {
+        return System::String::Equals(objectTag, tagToCompare, System::StringComparison::OrdinalIgnoreCase);
+    }
+
+    void Script::print(System::Object^ message)
+    {
+        System::Console::WriteLine(message);
+    }
+
+    // Internal lifecycle management - called by the engine
+    void Script::InternalAwake()
+    {
+        if (!awakeCalled && isEnabled)
+        {
+            awakeCalled = true;
+            Awake();
+        }
+    }
+
+    void Script::InternalStart()
+    {
+        if (!startCalled && isEnabled && awakeCalled)
+        {
+            startCalled = true;
+            Start();
+        }
+    }
+
+    void Script::InternalUpdate()
+    {
+        if (isEnabled && awakeCalled)
+        {
+            // Ensure Start is called before Update
+            if (!startCalled)
+            {
+                InternalStart();
+            }
+            Update();
+        }
+    }
+
+    void Script::InternalFixedUpdate()
+    {
+        if (isEnabled && awakeCalled && startCalled)
+        {
+            FixedUpdate();
+        }
+    }
+
+    void Script::InternalLateUpdate()
+    {
+        if (isEnabled && awakeCalled && startCalled)
+        {
+            LateUpdate();
+        }
+    }
+
+    void Script::InternalDestroy()
+    {
+        if (awakeCalled)
+        {
+            OnDestroy();
+        }
     }
 }
--- Survival_Kit/ScriptAPI/Script.hxx
+++ Survival_Kit/ScriptAPI/Script.hxx
@@ -6,14 +6,103 @@ namespace ScriptAPI
     public ref class Script abstract
     {
     public:
-        void virtual Update() {};
-        TransformComponent GetTransformComponent();
+        Script();
+        // Core lifecycle methods (virtual so they can be overridden)
+        virtual void Awake() {}
+        virtual void Start() {}
+        virtual void Update() {}
+        virtual void FixedUpdate() {}
+        virtual void LateUpdate() {}
+        virtual void OnDestroy() {}
+
+        // Enable/Disable lifecycle
+        virtual void OnEnable() {}
+        virtual void OnDisable() {}
+
+        // Common collision events
+        virtual void OnCollisionEnter() {}
+        virtual void OnCollisionExit() {}
+        virtual void OnCollisionStay() {}
+
+        // Trigger events
+        virtual void OnTriggerEnter() {}
+        virtual void OnTriggerExit() {}
+        virtual void OnTriggerStay() {}
+
+        // Mouse events
+        virtual void OnMouseEnter() {}
+        virtual void OnMouseExit() {}
+        virtual void OnMouseDown() {}
+        virtual void OnMouseUp() {}
+        virtual void OnMouseOver() {}
+        virtual void OnMouseDrag() {}
+
+        // Component access
+        TransformComponent^ GetTransformComponent();
+
+        // Properties
+        property bool enabled
+        {
+            bool get() { return isEnabled; }
+            void set(bool value)
+            {
+                if (isEnabled != value)
+                {
+                    isEnabled = value;
+                    if (value)
+                        OnEnable();
+                    else
+                        OnDisable();
+                }
+            }
+        }
+
+        property System::String^ name
+        {
+            System::String^ get() { return objectName; }
+            void set(System::String^ value) { objectName = value; }
+        }
+
+        property System::String^ tag
+        {
+            System::String^ get() { return objectTag; }
+            void set(System::String^ value) { objectTag = value; }
+        }
+
+        // Utility methods
+        bool CompareTag(System::String^ tagToCompare);
+
+        // Static utility
+        static void print(System::Object^ message);
 
     internal:
         void SetEntityId(int id);
+
+        // Internal lifecycle management - called by the engine
+        void InternalAwake();
+        void InternalStart();
+        void InternalUpdate();
+        void InternalFixedUpdate();
+        void InternalLateUpdate();
+        void InternalDestroy();
+
+        // State tracking properties
+        property bool hasAwakeCalled
+        {
+            bool get() { return awakeCalled; }
+        }
+
+        property bool hasStartCalled
+        {
+            bool get() { return startCalled; }
+        }
+
     private:
         int entityId;
+        bool isEnabled;
+        bool awakeCalled;
+        bool startCalled;
+        System::String^ objectName;
+        System::String^ objectTag;
     };
-
-
 }
